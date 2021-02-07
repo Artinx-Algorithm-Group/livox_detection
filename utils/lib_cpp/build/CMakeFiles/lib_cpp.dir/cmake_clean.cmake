fd9cd1cc9897fdcf084b852d89a2c836aeb0f4bc
Alvis Chan <Mingkai.chen.1@stonybrook.edu>
2021-02-06 23:21:34 -0800

    Init and modified project

---
+++ utils/lib_cpp/build/CMakeFiles/lib_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lib_cpp.dir/lib_cpp.cpp.o"
+  "lib_cpp.pdb"
+  "lib_cpp.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lib_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
